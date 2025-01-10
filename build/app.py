381aaaf8d8b569ac6fea54cbb4d5ea7c8e15778c
ofercraft <76131545+ofercraft@users.noreply.github.com>
2025-01-10 15:33:30 +0200

    Update app.py

--- build/app.py
+++ build/app.py
@@ -187,4 +187,5 @@ def student_profile():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
